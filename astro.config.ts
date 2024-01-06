e3035c96eb67e93e1b5279b937eaff95cd3fb083
Arvil Meña <arvilmena@gmail.com>
2024-01-06 08:10:36 +0800

    get astro example/demo files

---
+++ astro.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config';
+// https://astro.build/config
+export default defineConfig({
+  srcDir: './apps/frontend-astro/src',
+  publicDir: './apps/frontend-astro/public',
+  outDir: './dist/apps/frontend-astro',
+  integrations: [
+    (() => {
+      console.log(`my astro config loaded...`);
+      return {
+        name: 'project-name-vite-config',
+        hooks: {
+          'astro:config:setup': ({ updateConfig }) => {
+            updateConfig({
+              vite: {
+                server: {
+                  fs: {
+                    strict: false,
+                  },
+                },
+              },
+            });
+          },
+        },
+      };
+    })(),
+  ],
+});
